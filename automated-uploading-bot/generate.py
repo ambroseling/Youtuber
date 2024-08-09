fa3ffb161d58bfb4a12421316daa7feffd131bc2
ambroseling <lingambrose@gmail.com>
2024-08-09 01:51:09 -0400

    added new changes

--- automated-uploading-bot/generate.py
+++ automated-uploading-bot/generate.py
@@ -1,10 +1,13 @@
 
 import os
+import io
+from PIL import Image
 import numpy as np
 import srt
 import re
 import ffmpeg
 import whisperx
+import requests
 from whisperx.utils import get_writer
 import gc
 import soundfile as sf
@@ -18,8 +21,40 @@ import os
 import subprocess
 import nvidia.cublas.lib
 import nvidia.cudnn.lib
+import random
+
+# export LD_LIBRARY_PATH=/home/tiny_ling/anaconda3/lib/python3.11/site-packages/nvidia/cudnn/lib/  
+# conda install -n <your-env-name> cudnn=8.0.5 -c conda-forge
+# <path-to-conda-env>/lib/pythonX.X/site-packages/nvidia/cudnn/lib/libcudnn_ops_infer.so.8
+# 
 
 print(os.path.dirname(nvidia.cublas.lib.__file__) + ":" + os.path.dirname(nvidia.cudnn.lib.__file__))
+
+def get_audio_duration_ffmpeg(file_path):
+    result = subprocess.run(
+        ["ffprobe", "-v", "error", "-show_entries",
+         "format=duration", "-of",
+         "default=noprint_wrappers=1:nokey=1", file_path],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT
+    )
+    return float(result.stdout)
+
+
+def speed_up_audio(input_file, output_file, speed_factor):
+    # Construct the ffmpeg command
+    command = [
+        'ffmpeg',
+        '-i', input_file,       # Input file
+        '-filter:a', f"atempo={speed_factor}",  # Speed up audio
+        '-vn',                  # No video
+        '-y',                   # Overwrite output file
+        output_file
+    ]
+    
+    # Run the command
+    subprocess.run(command)
+
 
 def create_video(image_file, audio_file, subtitle_file, output_file):
     # FFmpeg command to combine image, audio, and subtitles into a video
@@ -96,17 +131,37 @@ def remove_overlapping_subtitles(file_path):
     with open(file_path, 'w', encoding='utf-8') as file:
         file.writelines(cleaned_lines)
 
+def generate_assets(hf_token,payload):
+    API_URL = "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev"
+    headers = {"Authorization": f"Bearer {hf_token}"}
+    response = requests.post(API_URL, headers=headers, json=payload)
+    content = response.content
+    return content
+
 def main(concept):
-    image_file = 'albert.png'               # Path to your image file
-    audio_file = 'example.wav'               # Path to your audio file
-    subtitle_file = 'example.ass'        # Path to your ASS subtitle file
+    if not os.path.isdir(f'{concept.replace(" ","_")}'):
+        os.mkdir(f'{concept.replace(" ","_")}')
+    audio_file = f'{concept.replace(" ","_")}.wav'               # Path to your audio file
+    audio_file = os.path.join(concept.replace(" ","_"),audio_file)
+    subtitle_file = f'{concept.replace(" ","_")}.ass'        # Path to your ASS subtitle file
+    subtitle_file = os.path.join(concept.replace(" ","_"),subtitle_file)
     output_file = f'{concept.replace(" ","_")}.mp4'       # Desired output video file path
+    output_file = os.path.join(concept.replace(" ","_"),output_file)
 
     load_dotenv(".env")
     api_key  = os.getenv("APIKEY")
+    hf_token = os.getenv("HF_TOKEN")
     client = Groq(
         api_key=api_key,
     )
+    funny_ppl = ["Barny the dinosaur", "Donald Trump", "Snoop dog", "Mickey Mouse","Goofy","Coward dog","Mr Bean","Bubble tubbies","LEGO batman","Kungfu panda"]
+    b_rolls = []
+    for i in range(3):
+        person = funny_ppl[random.randint(0, len(funny_ppl)-1)]
+        prompt = {"inputs":f"{person} on a computer coding with the screen saying: {concept}"}
+        image = Image.open(io.BytesIO(generate_assets(hf_token=hf_token,payload=prompt)))
+        image.save(os.path.join(concept.replace(" ","_"),f"b_roll_{i}.png"))
+
     device = "cuda" if torch.cuda.is_available() else "cpu"
     batch_size = 16 # reduce if low on GPU mem
     compute_type = "float16" # change to "int8" if low on GPU mem (may reduce accuracy)
@@ -174,25 +229,31 @@ def main(concept):
         full_audio = np.hstack((full_audio,audio_array))
 
     # import ipdb; ipdb.set_trace()
-    write("example.wav",sampling_rate,  data = (full_audio.T*32767).astype("int16"))
+    write(audio_file,sampling_rate,  data = (full_audio.T*32767).astype("int16"))
+    duration = get_audio_duration_ffmpeg(audio_file)
+    # import ipdb; ipdb.set_trace()
+    duration_target = 40
+    speed_factor = duration / duration_target
+    audio_file_new = os.path.join(f'{concept.replace(" ","_")}',f'{concept.replace(" ","_")}_new.wav')
+    speed_up_audio(audio_file,audio_file_new,speed_factor=speed_factor)
 
-    if os.path.exists("example.wav"):
-        audio = whisperx.load_audio("example.wav")
+    if os.path.exists(audio_file_new):
+        audio = whisperx.load_audio(audio_file_new)
         result = whisper_model.transcribe(audio, batch_size=batch_size)
         language_code=result["language"]
         model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
         result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
         result['language'] = language_code
-        vtt_writer = get_writer("srt", "captions/")
+        vtt_writer = get_writer("srt", f'{concept.replace(" ","_")}/')
         vtt_writer(
             result,
-            "example.wav",
+            audio_file_new,
             {"max_line_width": 15, "max_line_count": 1, "highlight_words": True},
         )
-        (ffmpeg.input("captions/example.srt").output('example.ass').run())
+        (ffmpeg.input(os.path.join(concept.replace(" ","_"),f'{concept.replace(" ","_")}_new.srt')).output(subtitle_file).run())
 
         # Usage
-        ass_name = str("example.ass")
+        # ass_name = os.path.join(concept.replace(" ","_"),f"{concept.replace(" ","_")}.ass")
         # we define the styles here:
         new_styles = {
             "Default": {
@@ -206,11 +267,10 @@ def main(concept):
                 17: "1"                 # Shadow (depth)
             }
         }
-        update_ass_styles(subtitle_file, f'{ass_name}', new_styles)
-        remove_overlapping_subtitles(f'{ass_name}')
-
+        update_ass_styles(subtitle_file, subtitle_file, new_styles)
+        remove_overlapping_subtitles(subtitle_file)
         try:
-            create_video(image_file, audio_file, subtitle_file, output_file)
+            create_video(f'{concept.replace(" ","_")}/b_roll_0.png' ,audio_file_new, subtitle_file, output_file)
             print(f"Video created successfully: {output_file}")
         except subprocess.CalledProcessError as e:
             print(f"An error occurred: {e}")
